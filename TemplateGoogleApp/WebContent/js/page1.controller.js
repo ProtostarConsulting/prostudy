ae643c350c1a905b076077b566d62c5b78d18137
ganeshlawande <ganesh.lawande@protostar.co.in>
2015-06-13 15:47:59 +0530

    all control to angular js

--- TemplateGoogleApp/WebContent/js/page1.controller.js
+++ TemplateGoogleApp/WebContent/js/page1.controller.js
@@ -4,85 +4,119 @@ function init() {
 
 }
 
-angular.module("myapp", []).controller(
-		"MyController",
-		[ '$scope', '$window',
-				function($scope, $window) {
-					$scope.fname = "Default Name"
-					$scope.serMsg = "";
-					$scope.$apply();
-					alert("Hi");
-					$scope.doClickCallSayHi = function() {
-						gapi.client.myhelloservice.say2Hi({
-							'myName2' : $scope.fname
-						}).execute(function(resp) {
-							alert(resp.data);
-							// $scope.serMsg = resp.data;
-							// alert(resp.data);
-							$scope.serMsg = resp.data;
-							$scope.$apply();
-						});
-					};
+angular
+		.module("myapp", [])
+		.controller(
+				"MyController",
+				[
+						'$scope',
+						'$window',
+						function($scope, $window) {
+							$scope.fname = "Default Name"
+							$scope.serMsg = "";
 
-					$scope.savePatient = function() {
-						gapi.client.patientservice.savePatient({
-							"firstName" : $scope.fname,
-							"lastName" : $scope.lname,
-							"city" : $scope.city
-						}).execute(function(resp) {
-							// alert(resp.data);
-							// $scope.serMsg = resp.data;
-							$("#formDiv").addClass('hidden');
-							$("#actionMsgDiv").removeClass('hidden');
-							console.log("Patient saved successfully!");
-						});
-					}
+							alert("Inside Controller");
 
-					$scope.addPatient = function() {
-						$scope.fname = "";
-						$scope.lname = "";
-						$scope.city = "";
-						$("#formDiv").removeClass('hidden');
-						$("#actionMsgDiv").addClass('hidden');
-					}
+							$scope.doClickCallSayHi = function() {
+								gapi.client.myhelloservice.say2Hi({
+									'myName2' : $scope.fname
+								}).execute(function(resp) {
+									alert(resp.data);
+									$scope.serMsg = resp.data;
+									$scope.$apply();
+								});
+							};
 
-					$scope.cancelBtn = function() {
-						document.location = '/index.html';
-					}
+							$scope.savePatient = function() {
+								gapi.client.patientservice.savePatient({
+									"firstName" : $scope.fname,
+									"lastName" : $scope.lname,
+									"city" : $scope.city
+								}).execute(function(resp) {
+									// alert(resp.data);
+									// $scope.serMsg = resp.data;
+									$("#formDiv").addClass('hidden');
+									$("#actionMsgDiv").removeClass('hidden');
+									console.log("Patient saved successfully!");
+								});
+							}
 
-					// New code in ctler
+							$scope.addPatient = function() {
+								$scope.fname = "";
+								$scope.lname = "";
+								$scope.city = "";
+								$("#formDiv").removeClass('hidden');
+								$("#actionMsgDiv").addClass('hidden');
+							}
 
-					$window.init = function() {
-//						$scope.$apply($scope.initgapi);
-						$scope.$apply($scope.load_guestbook_lib);
-						alert("Inside window.init");					
+							$scope.getPatientList = function() {
+								gapi.client.patientservice
+										.getAllPatients()
+										.execute(
+												function(resp) {
+													alert(resp.items[0].firstName);
+													// $scope.serMsg =
+													// resp.data;
+													$scope.items = resp.items;
 
-					};
+													console
+															.log("Got Patient list successfully!");
+													$scope.$apply(); // This
+																		// is
+																		// required
+																		// for
+																		// data
+																		// changed
+																		// in
+																		// hehind
+																		// in
+																		// controller
+																		// to be
+																		// applied
+																		// to UI
+												});
+							}
+							$scope.cancelBtn = function() {
+								document.location = '/index.html';
+							}
 
-										
-					$scope.load_guestbook_lib = function() {
-						var apiRoot = '//' + window.location.host + '/_ah/api';
+							// New code in ctler
 
-						// Loads the OAuth and helloworld APIs asynchronously, and
-						// triggers login
-						// when they have completed.
-						var apisToLoad;
+							$window.init = function() {
+								// $scope.$apply($scope.initgapi);
+								$scope.$apply($scope.load_guestbook_lib);
+								alert("Inside window.init");
 
-						var callback = function() {
-							console.log("Custom Service Loaded successfully!");
-						};
+							};
 
-						apisToLoad = 2; // must match number of calls to
-						// gapi.client.load()
-						gapi.client.load('myhelloservice', 'v0.1', function() {						
-							$scope.is_backend_ready = true;
-						    $scope.doClickCallSayHi();
-						}, apiRoot);
-						gapi.client.load('patientservice', 'v0.1', function() {
-							$scope.is_backend_ready = true;
-						}, apiRoot);
-							
-						};
+							$scope.load_guestbook_lib = function() {
+								var apiRoot = '//' + window.location.host
+										+ '/_ah/api';
 
-				} ]);
+								// Loads the OAuth and helloworld APIs
+								// asynchronously, and
+								// triggers login
+								// when they have completed.
+								var apisToLoad;
 
+								var callback = function() {
+									console
+											.log("Custom Service Loaded successfully!");
+								};
+
+								apisToLoad = 2; // must match number of calls to
+								// gapi.client.load()
+								gapi.client.load('myhelloservice', 'v0.1',
+										function() {
+											$scope.is_backend_ready = true;
+											$scope.doClickCallSayHi();
+										}, apiRoot);
+								gapi.client.load('patientservice', 'v0.1',
+										function() {
+											$scope.is_backend_ready = true;
+											$scope.getPatientList();
+										}, apiRoot);
+
+							};
+
+						} ]);
