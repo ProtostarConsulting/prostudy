84cc27cfbecf11be2b9a2f45b6d1b1ec0051f379
ganeshlawande <ganesh.lawande@protostar.co.in>
2015-07-19 19:15:12 +0530

    new Customer code

---
+++ BillingAndStockMangt/WebContent/app/cust/js/customer.ctr.js
@@ -0,0 +1,83 @@
+function init() {
+	// alert("Inside init");
+	window.initGAPI(); // Calls the init function defined on the window
+
+}
+
+app = angular.module("customerApp", [ 'ngMaterial', 'ngMessages' ]);
+app.controller("customerCtr", [
+		'$scope',
+		'$window',
+		function($scope, $window) {
+
+			$scope.loadCustomerList = function() {
+				console.log("loadCustomerList");
+				gapi.client.customerService.getAllCustomers().execute(
+						function(resp) {
+							console.log(resp);
+						});
+			};
+
+			$scope.addCustomer = function() {
+				console.log("in side addCustomer");
+				gapi.client.customerService.addCustomer($scope.cust)
+						.execute(function(resp) {
+							console.log("Add Customer Response: " + resp.msg);
+
+						})
+			};// end of call to addCustomer
+
+			$scope.newCustomer = function() {
+				return {
+					firstName : '',
+					lastName : '',
+					mobileNo : '',
+					email : '',
+					address : {
+						line1 : '',
+						line2 : '',
+						city : '',
+						state: '',
+						pin : '',	
+					}
+				};
+			}
+
+			$scope.cust = $scope.newCustomer();
+
+			$window.initGAPI = function() {
+				console.log("Came to initGAPI");
+				$scope.$apply($scope.loadCustomService);
+				//$scope.loadCustomService(); 
+				
+				//temp. Loading customer list here
+				$scope.loadCustomerList();
+
+			};
+
+			$scope.loadCustomService = function() {
+				console.log("Inside window.loadCustomServices");
+				var apiRoot = '//' + window.location.host + '/_ah/api';
+
+				// Loads the OAuth and helloworld APIs
+				// asynchronously, and
+				// triggers login
+				// when they have completed.
+				var apisToLoad;
+
+				apisToLoad = 1; // must match number of calls to
+				// gapi.client.load()
+
+				gapi.client.load('customerService', 'v0.1', function() {
+					$scope.is_backend_ready = true;
+					$scope.loadCustomerList
+
+				}, apiRoot);
+
+			};
+
+			// initialize local objects
+			$scope.customer = $scope.newCustomer();
+			$scope.customerList = {};
+
+		} ]);
