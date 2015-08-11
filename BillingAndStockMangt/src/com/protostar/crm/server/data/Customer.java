8fb2dbb1ad2a39a8a83cc3e5e913c9518f2b6858
ganeshlawande <ganesh.lawande@protostar.co.in>
2015-08-11 15:44:47 +0530

    removed data/service package

--- BillingAndStockMangt/src/com/protostar/crm/server/data/Customer.java
+++ BillingAndStockMangt/src/com/protostar/crm/server/data/Customer.java
@@ -46,9 +46,7 @@ public class Customer implements Serializable {
 	private String firstName;
 	private String lastName;
 	private String mobileNo;
-	private String email;
-	// private String address1;
-	// private String address2;
+	private String email;	
 	private String city;
 	private String pin;
 
@@ -92,12 +90,7 @@ public class Customer implements Serializable {
 		this.email = email;
 	}
 
-	/*
-	 * public String getAddress1() { return address1; } public void
-	 * setAddress1(String address1) { this.address1 = address1; } public String
-	 * getAddress2() { return address2; } public void setAddress2(String
-	 * address2) { this.address2 = address2; }
-	 */
+	
 	public String getCity() {
 		return city;
 	}
