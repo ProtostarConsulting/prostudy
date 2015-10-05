022cdbdb4e2445abd01ccc75693d3394e6a670bd
PriyaBharat <priyankabharat999@gmail.com>
2015-10-05 13:40:29 +0530

    missing icons changes

--- ProtostarWebsiteGAE/WebContent/index.ctr.js
+++ ProtostarWebsiteGAE/WebContent/index.ctr.js
@@ -4,13 +4,12 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
- 	
-
+  
   $scope.alert = '';
   $scope.showListBottomSheet = function($event) {
     $scope.alert = '';
     $mdBottomSheet.show({
-      template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
+      template: '<md-bottom-sheet class="md-list md-has-header"><div layout="row" layout-align="center start"> <md-card flex-gt-sm="160" flex-gt-md="120"> <md-card-content><h1><b>Contact Info</b></h1> <p>Protostar Consulting Services,<br>E-101, Manimangal Building,<br>Near Siddharth Motors,Old Pune Mumbai Road,<br> Kasarwadi, Pune, Maharashtra,PIN-411034 <br><br> Telephone: 020-30688468, Direct: +91 9922923988 <br> E-mail: info@protostar.co.in </p></md-list></md-bottom-sheet></md-card-content> </md-card></div>',
       controller: 'ListBottomSheetCtrl',
       targetEvent: $event
     }).then(function(clickedItem) {
