f7bebcd8007d004a98df0b83f37309658d001d93
ashvinigokale <ashvinigokale@gmail.com>
2015-11-07 18:09:34 +0530

    Updation of Locally stored Syllabus

--- prostudy/WebContent/app/syllabus/js/syllabus.js
+++ prostudy/WebContent/app/syllabus/js/syllabus.js
@@ -5,12 +5,14 @@ angular.module("prostudyApp").controller(
 				$log) {
 			$scope.isShowTable = true;
 			$scope.isShowRecord = false;
-
+			
+			//$scope.isUpdated=false;
 			$scope.showSavedToast = function() {
 				$mdToast.show($mdToast.simple().content('Syllabus Saved!')
 						.position("top").hideDelay(3000));
 			};
 			$scope.syllabus = "";
+			//$scope.uniqueid = 0;
 			$scope.selected = [];
 
 			$scope.query = {
@@ -60,6 +62,7 @@ angular.module("prostudyApp").controller(
 			}
 
 			$scope.tempSyllabus = {
+				syllabusId:"",
 				board : "",
 				standard : "",
 				subject : "",
@@ -70,24 +73,21 @@ angular.module("prostudyApp").controller(
 			$scope.syllabus1 = {};
 			$scope.syllabus2 = {};
 			$scope.syllabus3 = {};
+			
 			$scope.addSyllabus = function() {
 				$log.debug("No1");
 				var SyllabusService = appEndpointSF.getSyllabusService();
+				
 				SyllabusService.addSyllabus($scope.tempSyllabus).then(
 						function(msgBean) {
 							$log.debug("No6");
 							$log.debug("Inside Ctr addSyllabus");
 							$log.debug("msgBean.msg:" + msgBean.msg);
 							$scope.showSavedToast();
-							$scope.tempSyllabus = {
-								board : "",
-								standard : "",
-								subject : "",
-								chapterName : "",
-								chapterContent : ""
-							};
-						});
+
+					});
 				$log.debug("No4");
+				
 			}
 
 			$scope.getSyllabus = function() {
@@ -98,7 +98,7 @@ angular.module("prostudyApp").controller(
 					$scope.syllabus = syllabusList;
 				});
 			}
-
+			
 			$scope.getSyllabus();
 			// Function for getting Board depending on selection of board
 			$scope.getBoard = function(board) {
@@ -108,15 +108,16 @@ angular.module("prostudyApp").controller(
 						bb.push($scope.syllabus[i].standard);
 
 				}
-				$scope.stdsList = bb.filter(onlyUnique); // returns Unique
+				$scope.standardsList = bb.filter(onlyUnique); // returns Unique
 															// Standards
-				$log.debug(" StdList Value:" + $scope.stdsList);
+				$log.debug(" standardsList Value:" + $scope.standardsList);
 			};
 			// function for finding unique elements from array
 			function onlyUnique(value, index, self) {
 				return self.indexOf(value) === index;
 			}
-
+	
+			
 			// Function for getting Standard depending on selection of board
 			$scope.getStandard = function(board, standard) {
 
@@ -128,9 +129,12 @@ angular.module("prostudyApp").controller(
 
 				}
 
-				$scope.subList = bb.filter(onlyUnique); // returns Unique
+				$scope.subjectList = bb.filter(onlyUnique); // returns Unique
 														// Subject
-				$log.debug(" subList Value:" + $scope.subList);
+				$log.debug(" subjectList Value:" + $scope.subjectList);
+				
+				
+				
 
 			};
 			// Function for getting Subject depending on selection of board,
@@ -145,10 +149,9 @@ angular.module("prostudyApp").controller(
 						bb.push($scope.syllabus[i].chapterName);
 
 				}
-				$scope.chapterList = bb;
-				// $scope.chapterList = bb.filter( onlyUnique ); // returns
-				// Unique Chapters
+				$scope.chapterList = bb.filter( onlyUnique ); // returns Unique Chapters
 				$log.debug(" chapterList Value:" + $scope.chapterList);
+				
 
 			};
 
@@ -165,11 +168,10 @@ angular.module("prostudyApp").controller(
 						bb.push($scope.syllabus[i].chapterContent);
 
 				}
-				$scope.chapterContentList = bb;
-				// $scope.chapterContentList = bb.filter( onlyUnique ); //
-				// returns Unique Standards
+				$scope.chapterContentList = bb.filter( onlyUnique ); // returns Unique Standards
 				$log.debug(" chapterContentList Value:"
 						+ $scope.chapterContentList);
+				
 			};
 
 			// $scope.editRecord = create copy of selected[0];
@@ -185,17 +187,37 @@ angular.module("prostudyApp").controller(
 				$scope.isShowRecord = false;
 
 			}
-
-			$scope.save = function() {
+			
+			/*
+			$scope.update = function() {
 				for (var i = 0; i < $scope.syllabus.length; i++) {
 
 					if ($scope.syllabus[i].board == $scope.editRecord.board) {
 						$scope.syllabus[i] = $scope.editRecord;
-						$scope.selected = $scope.editRecord;
+						//$scope.sel = $scope.editRecord;
 						break;
 					}
-
-				}
+				}$scope.isShowTable = true;
+				$scope.isShowRecord = false;
+				
+				
+				
+				//$log.debug(" $scope.editRecord Value:"	+$scope.editRecord.subject);
+			}*/
+			
+			$scope.update = function() {
+				var SyllabusService = appEndpointSF.getSyllabusService();
+				
+				SyllabusService.updateSyllabus($scope.editRecord).then(
+						function(msgBean) {
+							$log.debug("No6");
+							$log.debug("Inside Ctr updateSyllabus");
+							$log.debug("msgBean.msg:" + msgBean.msg);
+							$scope.showSavedToast();
+					});
+				$log.debug("Select Syllabus updated");
+				
+				$log.debug("updated value"+$scope.syllabus.standard);
 				$scope.isShowTable = true;
 				$scope.isShowRecord = false;
 				
