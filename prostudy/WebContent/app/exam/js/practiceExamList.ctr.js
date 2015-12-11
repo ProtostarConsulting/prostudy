9a3d153fb0c5b826d9b4d4cac2a748b6fd77d634
PriyaBharat <priyankabharat999@gmail.com>
2015-12-11 15:51:17 +0530

    change to remove add feature

--- prostudy/WebContent/app/exam/js/practiceExamList.ctr.js
+++ prostudy/WebContent/app/exam/js/practiceExamList.ctr.js
@@ -1,22 +1,26 @@
-angular.module("prostudyApp")
+angular
+		.module("prostudyApp")
 		.controller(
 				"practiceExamListCtr",
 				function($scope, $window, $mdToast, $timeout, $mdSidenav,
 						$mdUtil, $log, $q, $sce, tableTestDataFactory,
-						appEndpointSF, $state, $filter,$stateParams) {
+						appEndpointSF, $state, $filter) {
 
 					$log.debug("Inside practiceExamListCtr");
 					$scope.showSavedToast = function() {
 						$mdToast.show($mdToast.simple()
-								.content('Added into MyPracticeExam!').position("top")
+								.content('Result Saved!').position("top")
 								.hideDelay(3000));
 					};
-			
-					
+
 					$scope.getPracticeExams = function() {
 
-						var PracticeExamService = appEndpointSF.getPracticeExamService();
-						PracticeExamService.getPracticeExams()	.then(function(practiceExamList) {
+						var PracticeExamService = appEndpointSF
+								.getPracticeExamService();
+						PracticeExamService
+								.getPracticeExams()
+								.then(
+										function(practiceExamList) {
 											$log
 													.debug("Inside Ctr getPracticeExam");
 											$scope.practiceTest = practiceExamList;
@@ -26,66 +30,20 @@ angular.module("prostudyApp")
 										});
 					}// End of getPracticeExams
 
-					$scope.getPracticeExams();
-					/*	
-					$log.debug("$stateParams:", $stateParams);
-					$log.debug("$stateParams.selectedMyExamId:",
-							$stateParams.selectedMyExamId);
-
+					$scope.addTestToMyList = function(selectedMyExamId) {
+						
+						$log.debug("selectedMyExamId:" + selectedMyExamId);
+						
+						var UserService = appEndpointSF.getUserService();
+						UserService.addMyPracticeExam(
+								UserService.getExamId(selectedMyExamId)).then(
+								function() {
+									$scope.showSavedToast();
+								});
+						
+					};
 					
-					$scope.selectedMyExamId = $stateParams.selectedMyExamId;
-				
-					$scope.showselectedExam = function() {
-						var PracticeExamService = appEndpointSF
-								.getPracticeExamService();
+					$scope.getPracticeExams();
 
-						PracticeExamService.getPracticeExamById(1)
-								.then(
-										function(practiceTest) {
-											$scope.TestSelected = practiceTest[0];
-										
-										});
-
-					}
-					$scope.showselectedExam();
-					$log.debug("$scope.TestSelected......:", $scope.TestSelected);
-					$scope.tempMyPracticeExam = {
-							examId : "",
-							examtitle : "",
-							board : "",
-							standard : "",
-							subject : "",
-							questions : [],
-							date : ""
-						};
-				
-					$scope.MyPracticeExam=[];
-						$scope.addMyPracticeExam = function() {$log.debug("No1");
-							var PracticeExamService = appEndpointSF.getPracticeExamService();
-							
-							$log.debug("$scope.TestSelected:", $scope.TestSelected);
-								$scope.MyPracticeExam.push($scope.TestSelected);
-						
-								PracticeExamService.addMyPracticeExam($scope.TestSelected)
-									.then(function(msgBean) {
-										$log.debug("No6");
-										$log.debug("Inside Ctr addMyPracticeExam");
-										$log.debug("msgBean.msg:" + msgBean.msg);
-										$scope.showSavedToast();
-										$scope.tempMYPracticeExam = {
-											examId : "",
-											examtitle : "",
-											board : "",
-											standard : "",
-											subject : "",
-											questions : [],
-											date : ""
-										};
-									});
-							$log.debug("No4");
-						}
-						
-					*/
-
-				});// end of practiceExamListCtr
+				});// end of examDemoCtr
 
