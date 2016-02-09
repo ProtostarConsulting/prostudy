52fdc614bb0e6b2189ea0f1222ae22450e1558bc
ganeshlawande <ganesh.lawande@protostar.co.in>
2016-02-09 14:12:21 +0530

    working date example

--- prostudy/WebContent/home.ctr.js
+++ prostudy/WebContent/home.ctr.js
@@ -1,126 +1,20 @@
-angular
-		.module("prostudyApp")
-		.controller(
-				"homeCtr",
-				[
-						'$scope',
-						'$window',
-						'$mdToast',
-						
-					
+angular.module("prostudyApp").controller(
+		"homeCtr",
+		function($scope, $window, $mdToast, $timeout, $mdSidenav, $mdUtil,
+				$log, $q, $location, objectFactory, appEndpointSF,
+				tableTestDataFactory) {
 
-						function($scope, $window, $mdToast, $timeout, $mdSidenav, $mdUtil,
-								$log, $q,$location, objectFactory, appEndpointSF,
-								tableTestDataFactory ) {
-							
-							/*$scope.loadAuth = function()
-							{
-								
-								console.log("inside Auth service data:" );
-								AuthService.logout();
-								$log.debug("inside Auth service data.data:" );
-							}
-							
-							$scope.loadAuth();*/
-							
-							/*$scope.loadUsernameList = function() {
-								console.log("inside getusernameList")
-								$scope.users = [];
-								$scope.selected = [];
-								tableTestDataFactory.getusernameList().then(
-										function(data) {
-											$scope.users = data;
-											$log.debug("inside ctr then $scope.getusernameList"
-													+ $scope.users);
-											console.log("inside getusernameList");
+			$scope.myDate = new Date();
 
-										});
+			$scope.showSimpleToast = function() {
+				$mdToast.show($mdToast.simple().content('X Saved!').position(
+						"top").hideDelay(3000));
+			};
 
-							}// end of loadInstituteList load
-							console.log("Inside homeCtr");
-							
-							$scope.loadUsernameList();
+			$scope.showDateValue = function() {
+				console.log("in side showDateValue");
+				$log.debug("$scope.myDate:" + $scope.myDate);
 
-							$scope.loginUser = function() {
-								var loggedin = false;
-								var totalUsers = $scope.users.length;
-								var usernameTyped = $scope.username;
-								var userpwdTyped = $scope.pwd;
+			};// end of call to addCustomer
 
-								for (i = 0; i < $scope.users.length; i++) {
-									if ($scope.users[i].username === usernameTyped
-											&& $scope.users[i].pwd === userpwdTyped) {
-										loggedin = true;
-										break;
-									}
-								}
-
-								if (loggedin === true) {
-									// alert("login successful");
-									$location.path("/home");
-								} else {
-									alert("username does not exist")
-								}
-							}*/
-							
-							
-							/*$scope.redirect = function()
-							{
-								alert("username does not exist")
-								$location.path("/login");
-								
-							}*/
-							
-							$scope.showSimpleToast = function() {
-								$mdToast.show($mdToast.simple().content(
-										'Customer Saved!').position("top")
-										.hideDelay(3000));
-							};
-
-							$scope.loadCustomerList = function() {
-								console.log("loadCustomerList");
-								gapi.client.customerservice.getAllCustomers()
-										.execute(function(resp) {
-											console.log(resp);
-										});
-							};
-
-							$scope.addCustomer = function() {
-								console.log("in side addCustomer");
-								gapi.client.customerservice
-										.addCustomer($scope.cust)
-										.execute(
-												function(resp) {
-													console
-															.log("Add Customer Response: "
-																	+ resp.msg);
-													$scope.showSimpleToast();
-													$scope.cust = $scope
-															.newCustomer();
-
-												})
-							};// end of call to addCustomer
-
-							$scope.newCustomer = function() {
-								return {
-									firstName : '',
-									lastName : '',
-									mobileNo : '',
-									email : '',
-									address : {
-										line1 : '',
-										line2 : '',
-										city : '',
-										state : '',
-										pin : '',
-									}
-								};
-							}
-
-							$scope.cust = $scope.newCustomer();
-
-							// initialize local objects
-							$scope.customer = $scope.newCustomer();
-							$scope.customerList = {};
-
-						} ]);
+		});
