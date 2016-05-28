7296928ea024362809117a8078d131370ce313aa
aniketbhalsing1 <aniketbhalsing1@gmail.com>
2016-05-28 17:53:23 +0530

    New GF Package

---
+++ prostudy/src/com/protostar/prostudy/gf/entity/GF_SrudentEntity.java
@@ -0,0 +1,8 @@
+package com.protostar.prostudy.gf.entity;
+
+import com.googlecode.objectify.annotation.Entity;
+
+@Entity
+public class GF_SrudentEntity {
+
+}
---
+++ prostudy/src/com/protostar/prostudy/gf/service/GF_StudentService.java
@@ -0,0 +1,9 @@
+package com.protostar.prostudy.gf.service;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiNamespace;
+
+@Api(name = "gfStudentService", version = "v0.1", namespace = @ApiNamespace(ownerDomain = "com.protostar.prostudy.gf.service", ownerName = "com.protostar.prostudy.gf.service", packagePath = ""))
+public class GF_StudentService {
+
+}
