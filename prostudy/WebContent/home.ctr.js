9004e84f95fde98cd2725e2f0978fb21bc88ace3
ganeshlawande <ganesh.lawande@protostar.co.in>
2016-07-07 13:51:55 +0530

    ng-file-uploading working code

--- prostudy/WebContent/home.ctr.js
+++ prostudy/WebContent/home.ctr.js
@@ -2,7 +2,7 @@ angular.module("prostudyApp").controller(
 		"homeCtr",
 		function($scope, $window, $mdToast, $timeout, $mdSidenav, $mdUtil,
 				$log, $q, $location, objectFactory, appEndpointSF,
-				tableTestDataFactory, $timeout, $interval) {
+				tableTestDataFactory, $timeout, $interval, Upload) {
 			$scope.loginCheck();
 			$scope.showSimpleToast = function() {
 				$mdToast.show($mdToast.simple().content('Customer Saved!')
@@ -63,7 +63,43 @@ angular.module("prostudyApp").controller(
 
 			};// end of call to addCustomer
 
+			$scope.submit = function() {
+				if ($scope.form.file.$valid && $scope.file) {
+					$scope.upload($scope.file);
+				}
+			};
 			
 			
+			//Test upload file code
+			$scope.username = "testuser220";
+
+			// upload on file select or drop
+			$scope.upload = function(file) {
+				Upload.upload({
+					url : 'uploadtestngfile',
+					data : {
+						file : file,
+						'username' : $scope.username
+					}
+				}).then(
+						function(resp) {
+							console.log('Success ' + resp.config.data.file.name
+									+ 'uploaded. Response from Servlet: ' + angular.toJson(resp.data));
+							$mdToast.show($mdToast.simple().content(
+							'Students Data Uploaded Sucessfully.').position("top")
+							.hideDelay(3000));
+						},
+						function(resp) {
+							console.log('Error status: ' + resp.status);
+						},
+						function(evt) {
+							var progressPercentage = parseInt(100.0
+									* evt.loaded / evt.total);
+							console.log('progress: ' + progressPercentage
+									+ '% ' + evt.config.data.file.name);
+						});
+			};
 			
+			//END Test upload file code
+
 		});
